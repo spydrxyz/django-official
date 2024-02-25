9d57a28e5f41a002bb1cacd999e16d8602f2e331
Shajjad Hossain <shajjad@tirzok.com>
2024-02-25 19:03:51 +0600

    Completed Chaptar 8

---
+++ officialApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OfficialappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'officialApp'
---
+++ officialApp/views.py
@@ -0,0 +1,51 @@
+from django.http import Http404
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, HttpResponse
+from django.views import generic
+from django.urls import reverse
+from django.utils import timezone
+
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = "officialApp/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+            :5
+        ]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "officialApp/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "officialApp/results.html"
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "officialApp/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("officialApp:results", args=(question.id,)))
